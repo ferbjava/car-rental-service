2a2a4ac79bf62c6a1b0d40d088733f6bbd31494d
Michal Kotecki <michal.kotecki@capgemini.com>
2019-02-22 08:40:02 +0100

    measter: initial commit

---
+++ src/app/models/car.model.ts
@@ -0,0 +1,11 @@
+export class Car {
+  id: number;
+  brand: string;
+  type: string;
+  productionYear: number;
+  color: string;
+  engineCapacity: number;
+  enginePower: number;
+  mileage: number;
+  giphyUrl: any;
+}
